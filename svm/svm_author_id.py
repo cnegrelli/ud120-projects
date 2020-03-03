430522b8f6aba24bbf363c2b06de44b770b8063b
Carolina <caro.negrelli@gmail.com>
2020-03-03 17:13:56 -0500

    Completed the svm script

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -21,10 +21,28 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 
-
 #########################################################
 ### your code goes here ###
 
+from sklearn import svm
+# clf=svm.SVC(kernel='linear')
+clf=svm.SVC(kernel='rbf', C=10000.0) # usarlo solo con una small sample, is too slow
+t0 = time() #set timer
+
+#reduce de dataset
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
+#
+
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s" #end timer
+t1 = time() #set timer
+print(clf.score(features_test,labels_test))
+print "training time:", round(time()-t1, 3), "s" #end timer
+
+pred = clf.predict(features_test)
+print(pred[10], pred[26], pred[50])
+print(sum(pred))
 #########################################################
 
 
